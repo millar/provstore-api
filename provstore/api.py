7282f65166979ebdbea233c6132142cdb0955134
Sam Millar <sam@rulica.com>
2014-07-19 22:31:07 +0100

    Add Travis config

--- provstore/api.py
+++ provstore/api.py
@@ -127,7 +127,7 @@ class Api(object):
         r = self._request('post', self.base_url + "/documents/%i/bundles/" % document_id,
                           data=json.dumps({
                             'content': json.loads(prov_bundle),
-                            'rec_id':  identifier
+                            'rec_id':  str(identifier)
                           }),
                           headers=headers)
 
